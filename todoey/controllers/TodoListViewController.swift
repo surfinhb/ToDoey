64b91db859770aecb03dd0a44062686ef20bdae5
Jake Shapiro <jtshapir@ucsc.edu>
2019-09-08 16:50:19 -0700

    items can be added and local data persistence with userdefaults

--- todoey/controllers/TodoListViewController.swift
+++ todoey/controllers/TodoListViewController.swift
@@ -10,13 +10,16 @@ import UIKit
 
 class ToDoListViewController: UITableViewController{
     
-    let itemArray = ["eggs", "waffles", "cereal"]
+    var itemArray = [String]()
+    let defaults = UserDefaults()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        if let items = defaults.array(forKey: "items") as? [String]{
+            itemArray = items
+        }
     }
-
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
@@ -30,7 +33,7 @@ class ToDoListViewController: UITableViewController{
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print(itemArray[indexPath.row])
+        
         let cell = tableView.cellForRow(at: indexPath)
         
         
@@ -40,8 +43,24 @@ class ToDoListViewController: UITableViewController{
             cell?.accessoryType = .checkmark
         }
     
-        
         tableView.deselectRow(at: indexPath, animated: true)
     }
+    
+    @IBAction func addItem(_ sender: Any) {
+        var enteredText = UITextField()
+        let alert = UIAlertController(title: "Add new todoey item", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add item", style: .default) { (UIAlertAction) in
+            self.itemArray.append(enteredText.text!)
+            self.defaults.setValue(self.itemArray, forKey: "items")
+            self.tableView.reloadData()
+        }
+        alert.addTextField { (textField) in
+            enteredText = textField
+        }
+        alert.addAction(action)
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    
 }
 
